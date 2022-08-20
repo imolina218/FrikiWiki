aee4b5feb98b64234efa8542eee69b174e3085a8
Ismael Molina <31362263+imolina218@users.noreply.github.com>
2022-08-20 17:31:26 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,122 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Friki Wiki - Home</title>
+        <meta name="home" content="Index of friki wiki"/>
+      </Head>
+
+      <header className={styles.header}>
+        <h1>Friki Wiki <span>Index</span></h1>
+      </header>
+
+      <main className={styles.container}>
+        <div className={styles.movies}>
+          <div className={styles.content}>
+            <p>Movies</p>
+            <p>Series</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p>Click to go to the wiki</p>
+          </div>
+          
+          <div className={styles.img}>
+            <Link href="/moviesandseries">
+              <Image
+                src="/img/MoviesAndSeries.png"
+                alt="Image of movies"
+                width={500}
+                height={800}
+              />  
+            </Link>
+          </div>
+
+        </div>
+
+        <div className={styles.marvel}>
+          <div className={styles.content}>
+            <p>Comics</p>
+            <p>Characters</p>
+            <p>Events</p>
+            <p>Series</p>
+            <p></p>
+            <p></p>
+            <p>Click to go to the wiki</p>
+          </div>
+          
+          <div className={styles.img}>
+            <Link href="/marvel">
+              <Image
+                src="/img/Marve.png"
+                alt="Image of Marvel"
+                width={500}
+                height={800}
+              />
+            </Link>
+          </div>
+
+        </div>
+
+        <div className={styles.starwars}>
+          <div className={styles.content}>
+            <p>Films</p>
+            <p>Characters</p>
+            <p>Starships</p>
+            <p>Vehicles</p>
+            <p>Species</p>
+            <p>Planets</p>
+            <p>Click to go to the wiki</p>
+          </div>
+          
+          <div className={styles.img}>
+            <Link href="/starwars">
+              <Image
+                src="/img/StarWars.jpg"
+                alt="Picture of Star Wars"
+                width={500}
+                height={800}
+              />
+            </Link>
+          </div>
+          
+        </div>
+
+        <div className={styles.rickandm}>
+          <div className={styles.content}>
+            <p>Episodes</p>
+            <p>Characters</p>
+            <p>Locations</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p>Click to go to the wiki</p>
+          </div>
+
+          <div className={styles.img}>
+            <Link href="/rickandmorty">
+              <Image
+                src="/img/RickAndMorty.jpg"
+                alt="Picture of Rick And Morty"
+                width={500}
+                height={800}
+              />
+            </Link>
+          </div>
+          
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>All rights not reserved</p>
+      </footer>
+    </>
+  )
+}
